1bfc976a1ccfff755df059f526b580bb51c60f44
Gaurav garg <garg.ncl@gmail.com>
2020-02-08 17:12:43 +0200

    Calculate BAC Value function

--- AlcoScan.py
+++ AlcoScan.py
@@ -1,14 +1,10 @@
-import bluetooth
+from bluetooth import *
 
+print ("performing inquiry...")
 
+nearby_devices = discover_devices(lookup_names = True)
 
-nearby_devices = bluetooth.discover_devices(
-        duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+print ("found %d devices" % len(nearby_devices))
 
-print("found %d devices" % len(nearby_devices))
-
-for addr, name in nearby_devices:
-    try:
-        print("  %s - %s" % (addr, name))
-    except UnicodeEncodeError:
-        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+for name, addr in nearby_devices:
+     print (" %s - %s" % (addr, name))
---
+++ FetchResponse.py
@@ -0,0 +1,33 @@
+def ReadRequest(requestStr):
+    if(requestStr[17:28]=="04030004FF00"):
+        return "Disconnected";
+    elif(requestStr[16:28]=="04030004FF01"):
+        return "Connected"
+    elif(requestStr[16:28]=="810300019000"):
+        return "Data Response"
+    elif(requestStr[16:28]=="810300029001"):
+        return "Start Blowing"
+    elif(requestStr[16:28]=="810300029002"):
+        return "finish blowing"
+    elif(requestStr[16:28]=="810300029003"):
+        return "Blowing Discontinue"
+    elif(requestStr[16:28]=="810300029004"):
+        return "Refuse Blowing"
+    elif(requestStr[16:28]=="810300029005"):
+        return "Measurement Result Completion"
+    elif(requestStr[16:26]=="8104000390"):
+        result=FetchResult(requestStr)
+        return "BAC Value is: "+str(result)+" %"
+    else:
+        return requestStr[28:30]
+
+
+
+def FetchResult(responseStr):
+    return (int(responseStr[28:30])*256+int(responseStr[26:28]))/100
+
+
+
+
+
+
